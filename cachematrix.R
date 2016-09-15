37ca66b0dd8eee6f7d30746d148c5addae985f52
ShubhoGhosal <shubhoghosal@yahoo.com>
2016-09-14 23:51:42 -0400

    Created body of functions
    Created the body of the makeCacheMatrix and cacheSolve functions and
    tested the functions.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Function that caches matrix in memory exploiting lexical scoping
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    i <- matrix()
+    set <- function(y) {
+        x <<- y
+        i <<- matrix()
+    }
+    get <- function() x
+    setinverse <- function(inverse) i<<- inverse
+    getinverse <- function() i
+    list(set = set, 
+         get = get, 
+         setinverse = setinverse, 
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Function that retrieves cached inverse of matrix if it exists
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    i <- x$getinverse()
+    if (!all(is.na(i))) {
+        message("Getting cached data")
+        return(i)
+    }
+    myMatrix <- x$get()
+    i <- solve(myMatrix, ...)
+    x$setinverse(i)
+    i
 }
